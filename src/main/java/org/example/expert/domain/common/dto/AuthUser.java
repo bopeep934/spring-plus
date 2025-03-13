a13480e38ebd553e651e3364a2d944ffeb3ff9ce
bopeep934 <gkinfn@gmail.com>
2025-03-13 19:31:53 +0900

    fix(user):Level1-2

--- src/main/java/org/example/expert/domain/common/dto/AuthUser.java
+++ src/main/java/org/example/expert/domain/common/dto/AuthUser.java
@@ -9,10 +9,12 @@ public class AuthUser {
     private final Long id;
     private final String email;
     private final UserRole userRole;
+    private final String nickname;
 
-    public AuthUser(Long id, String email, UserRole userRole) {
+    public AuthUser(Long id, String email, String nickname, UserRole userRole) {
         this.id = id;
         this.email = email;
+        this.nickname = nickname;
         this.userRole = userRole;
     }
 }
--- src/main/java/org/example/expert/domain/user/dto/response/UserResponse.java
+++ src/main/java/org/example/expert/domain/user/dto/response/UserResponse.java
@@ -7,9 +7,18 @@ public class UserResponse {
 
     private final Long id;
     private final String email;
-
+    private final String nickname;
     public UserResponse(Long id, String email) {
         this.id = id;
         this.email = email;
+        nickname="";
     }
+
+    public UserResponse(Long id, String email, String nickname) {
+        this.id = id;
+        this.email = email;
+        this.nickname = nickname;
+    }
+
+
 }
